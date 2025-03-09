3a3d32fa1f59cea6f775a5b387b436643478c0dd
m.sato <mst.nt.zsof.3.0@gmail.com>
2025-03-10 07:08:58 +0900

    ”マイグレーションとシーディングを実施”

--- src/database/seeders/ContactSeeder.php
+++ src/database/seeders/ContactSeeder.php
@@ -2,23 +2,18 @@
 
 namespace Database\Seeders;
 
-use Database\Factories\ContactFactory;
 use Illuminate\Database\Seeder;
 use App\Models\Contact;
 
-class DatabaseSeeder extends Seeder
+class ContactSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call([
-            CategorySeeder::class,
-        ]);
-
         Contact::factory()->count(35)->create();
     }
 }
